2a08eeebe96887c36951bd2022f22715d85c4861
0alp <alperenqwe@hotmail.com>
2024-07-09 01:18:26 +0300

    Update csproj files

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using http_client_c_sharp.HTTP;
+using System;
+using System.Net;
+using System.Text;
+using System.Threading.Channels;
+
+namespace http_client_c_sharp
+{
+    static class Program
+    {
+
+        static void Main(string[] argvs)
+        {
+           
+
+
+        }
+
+       
+
+
+    }
+
+
+    
+
+}
