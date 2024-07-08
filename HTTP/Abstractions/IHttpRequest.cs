c887867c92c71a76bd78c8d2227a072ea81badd4
0alp <alperenqwe@hotmail.com>
2024-07-09 00:51:31 +0300

    Create abstraction for http requests
    -An interface named IHttpRequest was created and an abstraction was made for potential http requests.

---
+++ HTTP/Abstractions/IHttpRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace http_client_c_sharp.HTTP.Abstractions
+{
+    internal interface IHttpRequest
+    {
+
+
+
+        StringBuilder BuildRequest(string host);
+
+
+
+
+
+
+
+
+    }
+}
