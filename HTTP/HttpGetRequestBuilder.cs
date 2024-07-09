414ccdda34a331f88a55476658c46bfa2f43415f
0alp <alperenqwe@hotmail.com>
2024-07-09 05:46:16 +0300

    Enhance readability and remove unnecessary code in HttpGetRequestBuilder.cs
    -Readability improved.
    -Fixed the error in the http format that gave the "505 - http version not supported" error.
    -Due to interface implementation requirements, return types outside of BuildRequest were converted to string.
    -Converted return types of BuildRequest methods to string as per interface implementation requirements, except for BuildRequest overloads that accept extra headers.

--- HTTP/HttpGetRequestBuilder.cs
+++ HTTP/HttpGetRequestBuilder.cs
@@ -12,23 +12,48 @@ namespace http_client_c_sharp.HTTP
     {
 
 
-        public StringBuilder BuildRequest(string host) 
+        public string BuildRequest(string host) 
         {
-
-            StringBuilder request = new StringBuilder();    
-            request.Append("GET / HTTP/1.1 \r\n" +
-                  $"Host: {host} \r\n");
-
+            return $"GET / HTTP/1.1\r\n"+
+                 $"Host: {host}\r\n";
+            
+            
+            
             
 
-            return request;
+            
         }
+        public string BuildRequest(string host, bool includeDefaultHeaders)
+        {
+            
+            string requestMessage = $"GET / HTTP/1.1\r\n" +
+                 $"Host: {host}\r\n";
+
+
+            if (includeDefaultHeaders)
+            {
+                requestMessage = requestMessage +
+                     "User-Agent: zeroalp/0.1 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0\r\n" +
+                     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r\n" +
+                     "Accept-Language: en-US,en;q=0.8,en-US;q=0.5,en;q=0.3\r\n" +
+                     "Accept-Encoding: gzip, deflate\r\n" +
+                     "Connection: keep-alive\r\n" +
+                     "Upgrade-Insecure-Requests: 1\r\n" +
+                     "Priority: u=1\r\n" +
+                     "\r\n";
+
+
+            }
+            
+            return requestMessage;
+        }
+
 
         public StringBuilder BuildRequest(string host, string httpVersion, Dictionary<string, string> headers = null)
         {
 
             StringBuilder request = new StringBuilder();
-            request.Append("GET / HTTP/1.1 \r\n" +
+            request.Append("GET / HTTP/1.1\r\n" +
                   $"Host: {host} \r\n");
 
             if (headers != null)
