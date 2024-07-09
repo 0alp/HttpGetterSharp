a244a92a5aab5aba1bf89238a3906a691d867ab1
0alp <alperenqwe@hotmail.com>
2024-07-09 06:01:07 +0300

    Improve code readability, remove unnecessary code, and resolve stream stalling issue
    -Fixed stream stalling issue by adding "if (!_stream.DataAvailable)" code block.
    -The ability to operate with domain names has been added.

--- HTTP/HttpConnectionService.cs
+++ HTTP/HttpConnectionService.cs
@@ -9,42 +9,50 @@ using System.Threading.Tasks;
 namespace http_client_c_sharp.HTTP
 {
     internal class HttpConnectionService
-    {   
-        
-        readonly IPAddress _address; 
+    {
+        readonly IPAddress _address;
         readonly int _port;
-        Stream _stream;
-        Stream _outputStream;
-        public string ResponseMessage {  get; set; }       
-        public HttpConnectionService(IPAddress ipAddress, int port)
+        NetworkStream _stream;
+        public string ResponseMessage { get; set; }
+        public HttpConnectionService(IPAddress ipAddress, int port = 80)
         {
-            _address = ipAddress;   
-            _port = port;   
+            _address = ipAddress;
+            _port = port;
+
+        }
+
+        public HttpConnectionService(string domainName, int port = 80)
+        {
+            
+            _address = Dns.GetHostAddresses(domainName)[0];
+            _port = port;
 
         }
 
 
         public async Task<bool> CreateHttpConnection()
         {
-            using (TcpClient client = new TcpClient())
+            TcpClient client;
+
+            client = new TcpClient();
+            try
             {
-                try
-                {
-                    await client.ConnectAsync(_address, _port);
-                    if (client.Connected)
-                        _stream = client.GetStream();
-                    return client.Connected;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error connecting: {ex.Message}");
-                    return false;
-                }
-
-
-
-
+                await client.ConnectAsync(_address, _port);
+                if (client.Connected)
+                    _stream = client.GetStream();
+                return client.Connected;
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error connecting: {ex.Message}");
+                return false;
+            }
+
+
+
+
+
+
 
         }
 
@@ -53,10 +61,10 @@ namespace http_client_c_sharp.HTTP
             if (_stream == null || !_stream.CanWrite)
                 throw new InvalidOperationException("Connection is not established or stream is not writable.");
 
-            byte[] buffer = Encoding.UTF8.GetBytes(requestMessage); 
+            byte[] buffer = Encoding.UTF8.GetBytes(requestMessage);
 
-            await _stream.WriteAsync(buffer, 0, buffer.Length);     
-            await _stream.FlushAsync(); 
+            await _stream.WriteAsync(buffer, 0, buffer.Length);
+            await _stream.FlushAsync();
 
 
 
@@ -65,28 +73,27 @@ namespace http_client_c_sharp.HTTP
         }
         public async Task<byte[]> ReceiveHttpResponse()
         {
+
             byte[] buffer = new byte[1024];
             int len;
             int totalBytesRead = 0;
-            if (_stream == null || _stream.CanRead)
+            if (_stream == null || !_stream.CanRead)
                 throw new InvalidOperationException("Connection is not established or stream is not readable.");
 
-            using (MemoryStream memoryStream = new MemoryStream())
+            while ((len = await _stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
             {
 
-                while ((len = await _stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-                {
-                    await _outputStream.WriteAsync(buffer, 0, len);
-                    await memoryStream.WriteAsync(buffer, 0, len);
-                    totalBytesRead += len;
-                }
-
-                return memoryStream.ToArray();  
-
+                await _stream.WriteAsync(buffer, 0, len);
+                totalBytesRead += len;
+                if (!_stream.DataAvailable)
+                    break;
             }
-            
-             
-            
+
+
+            return buffer;
+
+
+
 
         }
 
