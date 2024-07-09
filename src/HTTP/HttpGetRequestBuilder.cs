5eb419752ec68bd45df5853ff4441e660029247a
0alp <alperenqwe@hotmail.com>
2024-07-09 14:48:50 +0300

    Change return types of HttpGetRequestBuilder methods from string to StringBuilder
    -Switched to StringBuilder due to issues with string manipulation causing degradation and performance problems in GET requests.
    -Summary contents in BuildRequest methods refactored.

--- src/HTTP/HttpGetRequestBuilder.cs
+++ src/HTTP/HttpGetRequestBuilder.cs
@@ -15,59 +15,39 @@ namespace HttpGetterSharp.HTTP
     internal class HttpGetRequestBuilder : IHttpRequest
     {
 
+
         /// <summary>
-        /// Builds a basic HTTP GET request message with the specified host.
+        /// Builds an HTTP GET request message with the specified host.
+        /// Includes default headers such as User-Agent, Accept, Accept-Language, Accept-Encoding,
+        /// Connection, Upgrade-Insecure-Requests, and Priority.
         /// </summary>
         /// <param name="host">The host to include in the HTTP request message.</param>
-        /// <returns>The HTTP GET request message as a string.</returns>
-        public string BuildRequest(string host) 
+        /// <returns>The HTTP GET request message as a StringBuilder.</returns>
+        public StringBuilder BuildRequest(string host)
         {
-            return $"GET / HTTP/1.1\r\n"+
-                 $"Host: {host}\r\n";
-            
-            
-            
-            
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("GET / HTTP/1.1\r\n");
+            stringBuilder.Append($"Host: {host}\r\n");
 
-            
-        }
-        /// <summary>
-        /// Builds an HTTP GET request message with the specified host and optionally includes default headers.
-        /// </summary>
-        /// <param name="host">The host to include in the HTTP request message.</param>
-        /// <param name="includeDefaultHeaders">Indicates whether to include default headers in the HTTP request message.</param>
-        /// <returns>The HTTP GET request message as a string.</returns>
-        public string BuildRequest(string host, bool includeDefaultHeaders)
-        {
-            string requestMessage = $"GET / HTTP/1.1\r\n" +
-                 $"Host: {host}\r\n";
+            stringBuilder.AppendLine($"User-Agent: zeroalp/1.0 ({Environment.OSVersion.VersionString}; {(Environment.Is64BitOperatingSystem ? "x64" : "x86")}; rv:1.0) Gecko/20100101 zeroalp/1.0\r\n");
+            stringBuilder.AppendLine("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r\n");
+            stringBuilder.AppendLine($"Accept-Language: {CultureInfo.CurrentCulture.Name},en;q=0.8,en-US;q=0.5,en;q=0.3\r\n");
+            stringBuilder.AppendLine("Accept-Encoding: gzip, deflate\r\n");
+            stringBuilder.AppendLine("Connection: keep-alive\r\n");
+            stringBuilder.AppendLine("Upgrade-Insecure-Requests: 1\r\n");
+            stringBuilder.AppendLine("Priority: u=1\r\n");
 
-
-            if (includeDefaultHeaders)
-            {
-                requestMessage = requestMessage +
-                     $"User-Agent: zeroalp/0.1 ({Environment.OSVersion.VersionString}; {(Environment.Is64BitOperatingSystem ? "x64" : "x86")}; ) \r\n" +
-                     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r\n" +
-                     $"Accept-Language: {CultureInfo.CurrentCulture.Name},en;q=0.8,en-US;q=0.5,en;q=0.3\r\n" +
-                     "Accept-Encoding: gzip, deflate\r\n" +
-                     "Connection: keep-alive\r\n" +
-                     "Upgrade-Insecure-Requests: 1\r\n" +
-                     "Priority: u=1\r\n" +
-                     "\r\n";
-
-
-            }
-            
-            return requestMessage;
+            return stringBuilder;
         }
 
+
         /// <summary>
-        /// Builds an HTTP GET request message with the specified host, HTTP version, etc. optional headers.
+        /// Builds an HTTP GET request message with the specified host and optional headers.
         /// </summary>
         /// <param name="host">The host to include in the HTTP request message.</param>
         /// <param name="httpVersion">The HTTP version to include in the HTTP request message.</param>
         /// <param name="headers">Optional headers to include in the HTTP request message.</param>
-        /// <returns>The HTTP GET request message as a <see cref="StringBuilder"/>.</returns>
+        /// <returns>The HTTP GET request message as a StringBuilder.</returns>
         public StringBuilder BuildRequest(string host, string httpVersion, Dictionary<string, string> headers = null)
         {
 
