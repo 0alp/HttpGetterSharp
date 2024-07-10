2c298bf6a0cb7fcb0a0fe1a26e17c63070e73ea4
0alp <alperenqwe@hotmail.com>
2024-07-10 06:21:41 +0300

    Configure project for NuGet publishing

---
+++ src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace http_client_c_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+        
+        
+        
+        
+        }     
+
+
+    }
+}
